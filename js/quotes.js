971fc27146b76c7b2092cf350050a1301949365e
Peter-32 <peter@impactradius.com>
2017-07-15 15:14:06 -0700

    Added main files

---
+++ js/quotes.js
@@ -0,0 +1,79 @@
+// Quotes from: https://www.forbes.com/sites/kevinkruse/2013/05/28/inspirational-quotes/#6a64af7f6c7a
+var quotes = [
+  {
+    quote: "You miss 100% of the shots you don't take."
+    reference: 'Wayne Gretzky'
+  },
+  {
+    quote: "I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed."
+    reference: 'Michael Jordan'
+  },
+  {
+    quote: "The most difficult thing is the decision to act, the rest is merely tenacity."
+    reference: 'Amelia Earhart'
+  },
+  {
+    quote: "Every strike brings me closer to the next home run."
+    reference: 'Babe Ruth'
+  },
+  {
+    quote: "Definiteness of purpose is the starting point of all achievement."
+    reference: 'W. Clement Stone'
+  },
+  {
+    quote: "Life isn't about getting and having, it's about giving and being."
+    reference: 'Kevin Kruse'
+  },
+  {
+    quote: "Life is what happens to you while you're busy making other plans."
+    reference: 'John Lennon'
+  },
+  {
+    quote: "We become what we think about."
+    reference: 'Earl Nightingale'
+  },
+  {
+    quote: "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover."
+    reference: 'Mark Twain'
+  },
+  {
+    quote: "Life is 10% what happens to me and 90% of how I react to it."
+    reference: 'Charles Swindoll'
+  },
+  {
+    quote: "The most common way people give up their power is by thinking they don't have any."
+    reference: 'Alice Walker'
+  },
+  {
+    quote: "The best time to plant a tree was 20 years ago. The second best time is now."
+    reference: 'Chinese Proverb'
+  },
+  {
+    quote: "An unexamined life is not worth living."
+    reference: 'Socrates'
+  },
+  {
+    quote: "Eighty percent of success is showing up."
+    reference: 'Woody Allen'
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life."
+    reference: 'Steve Jobs'
+  },
+  {
+    quote: "Winning isn't everything, but wanting to win is."
+    reference: 'Vince Lombardi'
+  },
+  {
+    quote: "I am not a product of my circumstances. I am a product of my decisions."
+    reference: 'Stephen Covey'
+  },
+  {
+    quote: "Every child is an artist.  The problem is how to remain an artist once he grows up."
+    reference: 'Pablo Picasso'
+  },
+  {
+    quote: "You can never cross the ocean until you have the courage to lose sight of the shore."
+    reference: 'Christopher Columbus'
+  }
+]
