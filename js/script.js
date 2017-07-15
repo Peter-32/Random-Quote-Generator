0f27a396fb0caff2bf822e25e7bd7662aef9718d
Peter-32 <peter@impactradius.com>
2017-07-15 16:26:26 -0700

    Finished

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// Copy the array to a new variable.
+// This will hold the remaining quotes that haven't been shown.
+let remainingQuotes = quotes.slice();
+
 function print(message) {
   var outputDiv = document.getElementById("output");
   outputDiv.innerHTML = message;
@@ -7,11 +11,20 @@ function print(message) {
 
 // Return a random quote object.
 // There are 19 quote objects defined in the "quotes.js" file.
+// Loops through all 19 quotes before showing the same quotes again.
 function getRandomQuote() {
-  // Get a number inclusively between 0 and 18.
-  let randNumber = Math.floor(Math.random()*19)
-  // Return the quote at the random index.
-  return quotes[randNumber];
+  // If the quotes array is empty, restore the list.
+  if (remainingQuotes.length == 0) {
+      remainingQuotes = quotes.slice();
+  }
+  // Get a number inclusively between 0 and the last index of quotesLeft.
+  let randIdx = Math.floor(Math.random()*remainingQuotes.length)
+  // The quote that will be returned using the random index.
+  let quote = remainingQuotes[randIdx];
+  // Remove the quote from the list of quotes.
+  remainingQuotes.splice(randIdx, 1);
+  // Return the quote.
+  return quote;
 }
 
 // Gets a quote.
@@ -22,4 +35,8 @@ function printQuote() {
   <p class="source"> ` + randQuote.source+ `</p>`;
   quoteBox.innerHTML = message;
 }
+
+document.getElementById("button").addEventListener("click", function(){
+    printQuote();
+});
 printQuote();
